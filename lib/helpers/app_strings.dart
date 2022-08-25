c7dfca56061764c6d1eb9b660be8e5e6dac8ff58
duckniil <dania-spb@mail.ru>
2022-08-25 19:53:42 +0300

    Отметил, какие стринги для чего

--- lib/helpers/app_strings.dart
+++ lib/helpers/app_strings.dart
@@ -1,9 +1,11 @@
 /// Строковые константы приложения.
 class AppStrings {
-  static const appTitle = 'Это тайтл';
+  // SightListScreen. Список достопримечательностей.
   static const sightListAppBarTitle = 'Список\nинтересных мест';
+
+  // SightDetails. Детальная информация о достопримечательности.
   static const buildRouteText = 'построить маршрут';
   static const planText = 'Запланировать';
-  static const toFavorites = 'В Избранное';
+  static const toFavourites = 'В Избранное';
   static const closedTo = 'закрыто до';
 }
