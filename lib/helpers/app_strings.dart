9fc927fd1f5b397a93d502c03528d8e76dd33790
duckniil <dania-spb@mail.ru>
2022-08-28 22:49:08 +0300

    Строки для SightCard, VisitingScreen.

--- lib/helpers/app_strings.dart
+++ lib/helpers/app_strings.dart
@@ -14,8 +14,15 @@ class AppStrings {
   static const park = 'парк';
   static const museum = 'музей';
 
+  // SightCard. Карточка места.
+  static const planToVisit = 'Запланировано на';
+  static const placeVisited = 'Цель достигнута';
+
   // VisitingScreen. Список посещённых мест/мест к посещению.
   static const visitingScreenAppBarTitle = 'Избранное';
   static const wantToVisit = 'Хочу посетить';
   static const visited = 'Посетил';
+  static const empty = 'Пусто';
+  static const infoMarkLikedPlaces = 'Отмечайте понравившиеся\nместа и они появятся здесь.';
+  static const infoFinishRoute = 'Завершите маршрут,\nчтобы место попало сюда.';
 }
