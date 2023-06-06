68ebef298ab346157ec3f26f6ef1f9c8d00f2dc9
daniiliv <dania-spb@mail.ru>
2023-06-06 19:59:28 +0300

    refactor(избранные места): реализация репозитория избранных мест

---
+++ lib/favourite_places/data/favourite_place_data_repository.dart
@@ -0,0 +1,40 @@
+import 'package:places/domain/model/place.dart';
+import 'package:places/favourite_places/domain/favourite_place_repository.dart';
+
+// TODO(ivanovdale): Пока данные храним в переменной.
+final class FavouritePlaceDataRepository implements FavouritePlaceRepository {
+  final List<Place> _places = [];
+
+  @override
+  List<Place> getPlaces() {
+    return _places;
+  }
+
+  @override
+  List<Place> toggleFavourite(Place place) {
+    final isFavorite = _places.contains(place);
+    isFavorite ? _places.remove(place) : _places.add(place);
+
+    return _places;
+  }
+
+  @override
+  List<Place> removeFromFavourites(Place place) {
+    _places.remove(place);
+
+    return _places;
+  }
+
+  @override
+  List<Place> insertPlace(
+    Place place,
+    Place targetPlace,
+  ) {
+    final index = _places.indexOf(place);
+    final targetIndex = _places.indexOf(targetPlace);
+
+    return _places
+      ..removeAt(index)
+      ..insert(targetIndex, place);
+  }
+}
