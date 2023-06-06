0ee30915b5ecba7c64c30110a6393fd39dbe7502
daniiliv <dania-spb@mail.ru>
2023-06-06 20:17:25 +0300

    refactor(избранные места): внедрил cubit на экран

--- lib/favourite_places/presentation/widgets/visiting_place_list/base_visiting_place_list/base_visiting_place_list_state.dart
+++ lib/favourite_places/presentation/widgets/visiting_place_list/base_visiting_place_list/base_visiting_place_list_state.dart
@@ -2,15 +2,20 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:places/UI/screens/components/custom_pickers/custom_date_picker.dart';
 import 'package:places/UI/screens/components/custom_pickers/custom_time_picker.dart';
 import 'package:places/UI/screens/components/place_card/base_place_card.dart';
 import 'package:places/domain/model/place.dart';
-import 'package:places/favourite_places/widgets/cards/components/draggable_place_card_with_drag_target_option.dart';
-import 'package:places/favourite_places/widgets/cards/to_visit_place_card.dart';
-import 'package:places/favourite_places/widgets/cards/visited_place_card.dart';
-import 'package:places/favourite_places/widgets/visiting_place_list/base_visiting_place_list/base_visiting_place_list.dart';
+import 'package:places/favourite_places/presentation/bloc/favourite_place_cubit/favourite_place_cubit.dart';
+import 'package:places/favourite_places/presentation/bloc/favourite_places_bloc/favourite_places_bloc.dart';
+import 'package:places/favourite_places/presentation/bloc/favourite_places_bloc/favourite_places_event.dart';
+import 'package:places/favourite_places/presentation/widgets/cards/components/draggable_place_card_with_drag_target_option.dart';
+import 'package:places/favourite_places/presentation/widgets/cards/to_visit_place_card.dart';
+import 'package:places/favourite_places/presentation/widgets/cards/visited_place_card.dart';
+import 'package:places/favourite_places/presentation/widgets/visiting_place_list/base_visiting_place_list/base_visiting_place_list.dart';
+
 import 'package:places/helpers/app_assets.dart';
 import 'package:places/helpers/app_colors.dart';
 import 'package:places/helpers/app_strings.dart';
@@ -36,14 +41,12 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
     final listOfPlaces = widget.listOfPlaces;
 
     return listOfPlaces.isEmpty
-        ? widget.emptyVisitingList
+        ? Builder(builder: widget.emptyVisitingList)
         : ListView.builder(
             controller: _scrollController,
             itemCount: listOfPlaces.length,
             itemBuilder: (context, index) {
-              final placeCard = getPlaceCard(
-                listOfPlaces[index],
-              );
+              final place = listOfPlaces[index];
 
               return Stack(
                 children: [
@@ -52,20 +55,33 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
                   ),
                   Dismissible(
                     direction: DismissDirection.endToStart,
-                    onDismissed: (direction) => widget.deletePlaceFromList(
-                      listOfPlaces[index],
-                    ),
-                    key: ObjectKey(listOfPlaces[index]),
-                    child: DraggablePlaceCardWithDragTargetOption(
-                      index: index,
-                      placeCard: placeCard,
-                      isDragged: isDragged,
-                      onDragStarted: onDragStarted,
-                      onDragEnd: onDragEnd,
-                      onAccept: (data) =>
-                          widget.insertIntoPlaceList(index, data, context),
-                      scrollPlaceCardsWhenCardDragged:
-                          scrollPlaceCardsWhenCardDragged,
+                    onDismissed: (direction) =>
+                        context.read<FavouritePlacesBloc>().add(
+                              RemoveFromFavouritesEvent(place),
+                            ),
+                    key: ObjectKey(place),
+                    child: BlocProvider(
+                      create: (context) => FavouritePlaceCubit(place),
+                      child:
+                          BlocBuilder<FavouritePlaceCubit, FavouritePlaceState>(
+                        builder: (context, state) {
+                          final placeCard = getPlaceCard(context, place);
+                          final currentPlace = state.place;
+
+                          return DraggablePlaceCardWithDragTargetOption(
+                            index: index,
+                            place: currentPlace,
+                            placeCard: placeCard,
+                            isDragged: isDragged,
+                            onDragStarted: onDragStarted,
+                            onDragEnd: (details) => onDragEnd(),
+                            onAccept: (data) => widget.onPlaceInserted
+                                ?.call(data, currentPlace),
+                            scrollPlaceCardsWhenCardDragged:
+                                scrollPlaceCardsWhenCardDragged,
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ],
@@ -75,20 +91,20 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
   }
 
   /// Возвращает карточку места в зависимости от типа поля placeCardType.
-  BasePlaceCard getPlaceCard(Place place) {
+  BasePlaceCard getPlaceCard(BuildContext context, Place place) {
     return widget.placeCardType == ToVisitPlaceCard
         ? ToVisitPlaceCard(
             place,
             key: GlobalKey(),
-            onDeletePressed: () => widget.deletePlaceFromList(place),
-            onCalendarPressed: () => showToVisitDateTimePicker(place.id ?? 0),
+            onDeletePressed: () => widget.onPlaceDeleted?.call(place),
+            onCalendarPressed: () => showToVisitDateTimePicker(context, place),
           )
         : VisitedPlaceCard(
             place,
             key: GlobalKey(),
             // TODO(daniiliv): Вызов реальной функции.
             onSharePressed: () {},
-            onDeletePressed: () => widget.deletePlaceFromList(place),
+            onDeletePressed: widget.onPlaceDeleted,
           );
   }
 
@@ -122,36 +138,36 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
   }
 
   /// Устанавливает состояние - завершилось перетаскивание элемента.
-  void onDragEnd(DraggableDetails details) {
-    if (details.wasAccepted) {
-      setState(() {
-        isDragged = false;
-      });
-    }
+  void onDragEnd() {
+    setState(() {
+      isDragged = false;
+    });
   }
 
   /// Отображает пикеры для выбора даты и времени посещения места.
   /// Записывает выбранные дату и время.
-  Future<void> showToVisitDateTimePicker(int id) async {
+  Future<void> showToVisitDateTimePicker(
+    BuildContext context,
+    Place place,
+  ) async {
     // Вытащим сохранённую дату посещения из модели места.
     // Необходима при редактировании уже сохранённой даты посещения.
-    final savedToVisitDate = widget.viewModel.toVisitPlaces
-        .firstWhere((place) => place.id == id)
-        .visitDate;
+    final savedToVisitDate = place.visitDate;
 
     // В зависимости от платформы показать нативный пикер/пикеры.
     if (Platform.isAndroid) {
-      await showMaterialToVisitDateTimePickers(id, savedToVisitDate);
+      await showMaterialToVisitDateTimePickers(
+        context,
+        savedToVisitDate,
+      );
     } else {
       if (Platform.isIOS) {
-        await showCupertinoToVisitDateTimePicker(id, savedToVisitDate);
+        await showCupertinoToVisitDateTimePicker(
+          context,
+          savedToVisitDate,
+        );
       }
     }
-  }
-
-  /// Обновляет дату посещения во вьюмодели места.
-  void updateViewModelPlaceVisitDateTime(int id, DateTime pickedDateTime) {
-    widget.viewModel.updateToVisitPlaceDateTime(id, pickedDateTime);
   }
 
   /// Отображает пикер для выбора даты в стиле Material.
@@ -212,7 +228,7 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
   /// Отображает пикеры даты и времени в стиле Material.
   /// Обновляет вьюмодель места.
   Future<void> showMaterialToVisitDateTimePickers(
-    int id,
+    BuildContext context,
     DateTime? savedToVisitDate,
   ) async {
     final pickedDate = await showMaterialToVisitDatePicker(savedToVisitDate);
@@ -227,13 +243,17 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
         pickedTime?.minute ?? 0,
       );
 
-      updateViewModelPlaceVisitDateTime(id, pickedDateTime);
+      if (context.mounted) {
+        context
+            .read<FavouritePlaceCubit>()
+            .updateToVisitPlaceDateTime(pickedDateTime);
+      }
     }
   }
 
   /// Отображает пикер даты и времени в стиле Cupertino.
   Future<void> showCupertinoToVisitDateTimePicker(
-    int id,
+    BuildContext context,
     DateTime? savedToVisitDate,
   ) {
     return showModalBottomSheet<void>(
@@ -241,7 +261,7 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
         maxHeight: MediaQuery.of(context).size.height * 0.3,
       ),
       context: context,
-      builder: (context) {
+      builder: (_) {
         final currentDateTime = DateTime.now();
         final initialDate =
             getPickerInitialDate(currentDateTime, savedToVisitDate);
@@ -250,8 +270,9 @@ class BaseVisitingPlaceListState extends State<BaseVisitingPlaceList> {
           minimumDate: currentDateTime,
           maximumDate: currentDateTime.add(const Duration(days: 100)),
           initialDateTime: initialDate,
-          onDateTimeChanged: (pickedDateTime) =>
-              updateViewModelPlaceVisitDateTime(id, pickedDateTime),
+          onDateTimeChanged: (pickedDateTime) => context
+              .read<FavouritePlaceCubit>()
+              .updateToVisitPlaceDateTime(pickedDateTime),
         );
       },
     );
