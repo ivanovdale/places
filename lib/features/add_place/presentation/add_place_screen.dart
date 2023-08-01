bd6c4149b03933c7fc1da01a59e20984dee2ad1a
ivanovdale <dania-spb@mail.ru>
2023-08-01 14:42:18 +0300

    feat(внедрение elementary): реализовал ElementaryWidget

--- lib/features/add_place/presentation/add_place_screen.dart
+++ lib/features/add_place/presentation/add_place_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 import 'package:places/UI/screens/components/custom_app_bar.dart';
 import 'package:places/domain/model/place.dart';
+import 'package:places/features/add_place/presentation/add_place_wm.dart';
 import 'package:places/features/add_place/presentation/widgets/buttons/cancel_button.dart';
 import 'package:places/features/add_place/presentation/widgets/buttons/create_button.dart';
 import 'package:places/features/add_place/presentation/widgets/buttons/mark_on_map_button.dart';
@@ -14,131 +18,48 @@ import 'package:places/features/add_place/presentation/widgets/text_fields/descr
 import 'package:places/features/add_place/presentation/widgets/text_fields/map_text_fields.dart';
 import 'package:places/features/add_place/presentation/widgets/text_fields/name_text_field.dart';
 import 'package:places/helpers/app_strings.dart';
-import 'package:places/mocks.dart' as mocked;
 import 'package:places/utils/string_extension.dart';
 
 /// Экран для добавления нового места.
 ///
 /// Позволяет выбрать категорию места, ввести его название, описание и географические координаты.
 /// Также координаты можно установить, указав точку на карте.
-class AddPlaceScreen extends StatelessWidget {
-  const AddPlaceScreen({Key? key}) : super(key: key);
+class AddPlaceScreen extends ElementaryWidget<AddPlaceWM> {
+  const AddPlaceScreen({super.key}) : super(createAddPlaceWM);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(AddPlaceWM wm) {
     return GestureDetector(
-      onTap: () {
-        final currentFocus = FocusScope.of(context);
-
-        if (!currentFocus.hasPrimaryFocus) {
-          currentFocus.unfocus();
-        }
-      },
+      onTap: wm.onFormTap,
       child: Scaffold(
         appBar: CustomAppBar(
           title: AppStrings.newPlace.capitalize(),
-          titleTextStyle: Theme.of(context).textTheme.titleMedium,
+          titleTextStyle: wm.appBarTextStyle,
           centerTitle: true,
           toolbarHeight: 56,
-          leading: const CancelButton(),
+          leading: CancelButton(
+            onCancelButtonPressed: wm.onCancelButtonPressed,
+          ),
           leadingWidth: 73,
         ),
-        body: const _AddPlaceBody(),
+        body: AddPlaceBody(
+          wm: wm,
+        ),
       ),
     );
-  }
-}
-
-/// Прокидывает данные [data] вниз по дереву.
-/// Всегда оповещает дочерние виджеты о перерисовке.
-class InheritedAddPlaceBodyState extends InheritedWidget {
-  static AddPlaceBodyState of(BuildContext context) {
-    return (context.dependOnInheritedWidgetOfExactType<
-            InheritedAddPlaceBodyState>() as InheritedAddPlaceBodyState)
-        .data;
-  }
-
-  final AddPlaceBodyState data;
-
-  const InheritedAddPlaceBodyState({
-    Key? key,
-    required Widget child,
-    required this.data,
-  }) : super(key: key, child: child);
-
-  @override
-  bool updateShouldNotify(InheritedAddPlaceBodyState oldWidget) {
-    return true;
   }
 }
 
 /// Отображает свойства добавленяемого места - категория, название, описание, координаты.
 /// Позволяет ввести географические координаты места, указав точку на карте.
 /// Имеет кнопку "Создать" для добавления нового места.
-class _AddPlaceBody extends StatefulWidget {
-  const _AddPlaceBody({Key? key}) : super(key: key);
+class AddPlaceBody extends StatelessWidget {
+  final AddPlaceWM wm;
 
-  @override
-  State<_AddPlaceBody> createState() => AddPlaceBodyState();
-}
-
-/// Состояние виджета для добавления нового места.
-///
-/// Содержит в себе всю верстку, контроллеры и фокусноды для полей ввода.
-/// Также содержит ключ формы для валидации полей.
-/// Хранит выбранный тип места.
-class AddPlaceBodyState extends State<_AddPlaceBody> {
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController latitudeController = TextEditingController();
-  final TextEditingController longitudeController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
-  final FocusNode nameFocusNode = FocusNode();
-  final FocusNode latitudeFocusNode = FocusNode();
-  final FocusNode longitudeFocusNode = FocusNode();
-  final FocusNode descriptionFocusNode = FocusNode();
-
-  /// Для валидации координат места.
-  final formKey = GlobalKey<FormState>();
-  PlaceTypes? selectedPlaceType;
-
-  /// Список добавляемых фото.
-  late List<String> newPhotoList = [];
-
-  @override
-  void initState() {
-    super.initState();
-
-    // TODO(daniiliv): инициализация списка добавляемых фото моковыми данными.
-    newPhotoList = mocked.photoCarouselOnAddPlaceScreen;
-
-    // Для обновления SuffixIcon в TextField.
-    nameFocusNode.addListener(() {
-      setState(() {});
-    });
-    latitudeFocusNode.addListener(() {
-      setState(() {});
-    });
-    longitudeFocusNode.addListener(() {
-      setState(() {});
-    });
-    descriptionFocusNode.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    nameController.dispose();
-    latitudeController.dispose();
-    longitudeController.dispose();
-    descriptionController.dispose();
-    nameFocusNode.dispose();
-    latitudeFocusNode.dispose();
-    longitudeFocusNode.dispose();
-    descriptionFocusNode.dispose();
-  }
+  const AddPlaceBody({
+    super.key,
+    required this.wm,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -147,50 +68,66 @@ class AddPlaceBodyState extends State<_AddPlaceBody> {
       bottom: 12.0,
     );
 
-    return SingleChildScrollView(
-      child: Form(
-        key: formKey,
-        child: InheritedAddPlaceBodyState(
-          data: this,
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              PhotoCarousel(),
-              PlaceTypeLabel(),
-              PlaceTypeSelectionField(),
-              PlaceTypePaddedDivider(),
-              NameLabel(padding: defaultLabelPadding),
-              NameTextField(),
-              MapTextFields(),
-              MarkOnMapButton(),
-              DescriptionLabel(padding: defaultLabelPadding),
-              DescriptionTextField(),
-              CreateButton(),
-            ],
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Form(
+              key: wm.formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ValueListenableBuilder<List<String>>(
+                    valueListenable: wm.newPhotoList,
+                    builder: (_, data, __) {
+                      return PhotoCarousel(
+                        newPhotoList: data,
+                        onAddNewPhotoPressed: wm.addPhotoToList,
+                        onDeletePhotoPressed: wm.deletePhotoFromList,
+                      );
+                    },
+                  ),
+                  const PlaceTypeLabel(),
+                  ValueListenableBuilder<PlaceTypes?>(
+                    valueListenable: wm.placeType,
+                    builder: (_, data, __) {
+                      return PlaceTypeSelectionField(
+                        placeType: data,
+                        onPlaceTypeSelected: wm.setPlaceType,
+                      );
+                    },
+                  ),
+                  const PlaceTypePaddedDivider(),
+                  const NameLabel(padding: defaultLabelPadding),
+                  NameTextField(
+                    nameController: wm.nameController,
+                    nameFocusNode: wm.nameFocusNode,
+                    latitudeFocusNode: wm.latitudeFocusNode,
+                  ),
+                  MapTextFields(
+                    latitudeController: wm.latitudeController,
+                    longitudeController: wm.longitudeController,
+                    latitudeFocusNode: wm.latitudeFocusNode,
+                    longitudeFocusNode: wm.longitudeFocusNode,
+                    descriptionFocusNode: wm.descriptionFocusNode,
+                  ),
+                  const MarkOnMapButton(),
+                  const DescriptionLabel(padding: defaultLabelPadding),
+                  DescriptionTextField(
+                    descriptionController: wm.descriptionController,
+                    descriptionFocusNode: wm.descriptionFocusNode,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
-      ),
+        SafeArea(
+          child: CreateButton(
+            onButtonPressed: () => unawaited(wm.createNewPlace()),
+          ),
+        ),
+      ],
     );
-  }
-
-  /// Устанавливает выбранный тип места.
-  void setSelectedPlaceType(PlaceTypes selectedPlaceType) {
-    setState(() {
-      this.selectedPlaceType = selectedPlaceType;
-    });
-  }
-
-  /// Добавляет новое фото в список добавляемых фото.
-  void addPhotoToList(String photoUrl) {
-    setState(() {
-      newPhotoList.add(photoUrl);
-    });
-  }
-
-  /// Удаляет фото из списка добавляемых фото.
-  void deletePhotoFromList(int index) {
-    setState(() {
-      newPhotoList.removeAt(index);
-    });
   }
 }
