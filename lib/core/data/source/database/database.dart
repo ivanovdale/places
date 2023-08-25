bc8c70508639a3488a47acc110fd89da5774ac6a
ivanovdale <dania-spb@mail.ru>
2023-08-25 17:28:05 +0300

    feat(избранное): написал sql-запросы и методы для работы с бд

--- lib/core/data/source/database/database.dart
+++ lib/core/data/source/database/database.dart
@@ -1,7 +1,9 @@
 import 'package:places/core/data/source/database/database_impl.dart';
+import 'package:places/features/favourite_places/data/dao/favourite_dao.dart';
 import 'package:places/features/place_search/data/dao/search_history_item_dao.dart';
 
 abstract interface class Database {
+  //# region История поиска.
   Stream<List<SearchHistoryItemDao>> getSearchHistory();
 
   Future<SearchHistoryItemDao?> getSearchHistoryItemById(int id);
@@ -13,4 +15,25 @@ abstract interface class Database {
   Future<void> removeFromSearchHistory(int id);
 
   Future<void> clearSearchHistory();
+
+  //# endregion
+
+  //# region Избранное.
+  Stream<List<FavouriteDao>> getFavourites();
+
+  Future<FavouriteDao?> getFavouriteById(int id);
+
+  Future<void> addToFavourites(FavouritesCompanion favourite);
+
+  /// [visited] - bool 0 или 1.
+  Future<void> updateFavouriteVisited(int id, int visited);
+
+  /// [date] - дата в unix формате.
+  Future<void> updateFavouriteDate(int id, int date);
+
+  Future<void> updateFavouritePosition(int id, int position);
+
+  Future<void> deleteFavourite(int id);
+
+//# endregion
 }
--- lib/core/data/source/database/database_impl.dart
+++ lib/core/data/source/database/database_impl.dart
@@ -1,13 +1,14 @@
 import 'package:drift/drift.dart';
 import 'package:places/core/data/source/database/connection.dart' as impl;
 import 'package:places/core/data/source/database/database.dart';
+import 'package:places/features/favourite_places/data/dao/favourite_dao.dart';
 import 'package:places/features/place_search/data/dao/search_history_item_dao.dart';
 
 part 'database_impl.g.dart';
 
 /// Для генерации класса запустить команду
 /// dart run build_runner build
-@DriftDatabase(include: {'sql/search_history.drift'})
+@DriftDatabase(include: {'sql/search_history.drift', 'sql/favourites.drift'})
 class DatabaseImpl extends _$DatabaseImpl implements Database {
   DatabaseImpl({
     required String dbName,
@@ -20,8 +21,18 @@ class DatabaseImpl extends _$DatabaseImpl implements Database {
         );
 
   @override
-  int get schemaVersion => 2;
+  int get schemaVersion => 3;
 
+  @override
+  MigrationStrategy get migration => MigrationStrategy(
+        onUpgrade: (migrator, from, to) async {
+          if (from == 2) {
+            await migrator.createTable(favourites);
+          }
+        },
+      );
+
+  //# region История поиска.
   @override
   Stream<List<SearchHistoryItemDao>> getSearchHistory() =>
       _getSearchHistory().watch();
@@ -45,4 +56,42 @@ class DatabaseImpl extends _$DatabaseImpl implements Database {
 
   @override
   Future<void> clearSearchHistory() => transaction(_deleteAllSearchHistory);
+
+  //# endregion
+
+  //# region Избранное.
+  @override
+  Stream<List<FavouriteDao>> getFavourites() => _getFavourites().watch();
+
+  @override
+  Future<FavouriteDao?> getFavouriteById(int id) =>
+      _getFavouriteById(id).getSingleOrNull();
+
+  @override
+  Future<void> addToFavourites(FavouritesCompanion favourite) =>
+      _insertFavourite(
+        favourite.id.value,
+        favourite.name.value,
+        favourite.imageUrl.value,
+        favourite.type.value,
+        favourite.date.value,
+        favourite.visited.value,
+      );
+
+  @override
+  Future<void> updateFavouriteVisited(int id, int visited) =>
+      transaction(() => _updateFavouriteVisited(visited, id));
+
+  @override
+  Future<void> updateFavouriteDate(int id, int date) =>
+      transaction(() => _updateFavouriteDate(date, id));
+
+  @override
+  Future<void> updateFavouritePosition(int id, int position) =>
+      transaction(() => _updateFavouritePosition(position, id));
+
+  @override
+  Future<void> deleteFavourite(int id) => _deleteFavourite(id);
+
+//# endregion
 }
