a1d7d40b39f16397244a0a4ed291118ba014cb0c
ivanovdale <dania-spb@mail.ru>
2023-09-21 12:24:18 +0300

    build: внедрил и настроил либу yandex_mapkit

--- android/app/src/main/kotlin/ru/ivanovd/places/MainActivity.kt
+++ android/app/src/main/kotlin/ru/ivanovd/places/MainActivity.kt
@@ -1,6 +1,14 @@
 package ru.ivanovd.places
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("c3acded2-d800-409d-8f13-1e6550780e67")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey("c3acded2-d800-409d-8f13-1e6550780e67")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
