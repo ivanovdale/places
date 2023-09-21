a4a07f3d657f2573ad7adca0bd69e28fbc6df682
ivanovdale <dania-spb@mail.ru>
2023-09-21 16:53:01 +0300

    fix(фильтры мест): изменил константу значения небольших экранов для исправления верстки

--- lib/core/utils/screen_util.dart
+++ lib/core/utils/screen_util.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 extension ScreenUtil on BuildContext {
-  static const _smallScreenWidth = 332;
+  static const _smallScreenWidth = 375;
 
   bool get isSmallScreenWidth =>
       MediaQuery.sizeOf(this).width <= _smallScreenWidth;
