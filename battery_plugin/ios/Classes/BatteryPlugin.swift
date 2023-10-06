735ef034685f081b987175b6e9f93825bdfeae5b
ivanovdale <dania-spb@mail.ru>
2023-10-06 16:24:05 +0300

    feat(battery plugin): реализовал ios часть

--- battery_plugin/ios/Classes/BatteryPlugin.swift
+++ battery_plugin/ios/Classes/BatteryPlugin.swift
@@ -1,19 +1,50 @@
 import Flutter
-import UIKit
 
-public class BatteryPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "battery_plugin", binaryMessenger: registrar.messenger())
-    let instance = BatteryPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+public class BatteryPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    var eventSink: FlutterEventSink?
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = BatteryPlugin()
+        let batteryLevelChannel = FlutterEventChannel(name: "battery_plugin", binaryMessenger: registrar.messenger())
+        batteryLevelChannel.setStreamHandler(instance)
     }
-  }
+
+    private func getBatteryLevel() -> Int {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        if UIDevice.current.batteryState == .unknown {
+            return -1
+        } else {
+            return Int(UIDevice.current.batteryLevel * 100)
+        }
+    }
+
+    public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = eventSink
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        sendBatteryLevelEvent()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(onBatteryLevelDidChange(_:)), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
+
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        NotificationCenter.default.removeObserver(self)
+        eventSink = nil
+        return nil
+    }
+
+    @objc private func onBatteryLevelDidChange(_ notification: Notification) {
+        sendBatteryLevelEvent()
+    }
+
+    private func sendBatteryLevelEvent() {
+        guard let eventSink = eventSink else {
+            return
+        }
+
+        let state = getBatteryLevel()
+        eventSink(state)
+    }
 }
